cd1262b485be56815c26592c2e8e95bc97f52e36
Salapuddin <vsalapuddin@ufl.edu>
2023-01-16 12:36:47 -0500

    finished skills section, started projects

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Skill from "./Skill";
 
 type Props = {};
 
 function Skills({}: Props) {
   return (
-    <motion.div className="h-screen flex relative flex-col text-center">
-      <h3>Skills</h3>
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1 }}
+      className="h-screen flex relative flex-col text-center md:text-left xl:flex-row 
+    max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Skills
+      </h3>
+
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+        Hover over Skills
+      </h3>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
     </motion.div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -24,6 +25,9 @@ const Home: NextPage = () => {
       </section>
       <section id="skills" className="snap-center">
         <Skills />
+      </section>
+      <section id="projects" className="snap-center">
+        <Projects />
       </section>
     </div>
   );
