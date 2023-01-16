c8fcec42913e1cc61e0d2246dffbf77ffec01be9
Salapuddin <vsalapuddin@ufl.edu>
2023-01-15 22:46:28 -0500

    hero animations and format

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function About({}: Props) {
+  return (
+    <header className="p-5 flex items-start justify-between ">
+      <div>About</div>
+    </header>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/Link";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  return (
+    <motion.div
+      initial={{ y: -500, opacity: 0, scale: 1 }}
+      animate={{ y: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex flex-col space-y-8 items-center justify-center"
+    >
+      <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+        Web Developer
+      </h2>
+      <h1 className="text-5xl lg:text-6xl font-semibold -px-10">
+        Vince Salapuddin
+      </h1>
+
+      <div className="pt-5">
+        <Link href="#about">
+          <button className="heroButton">About</button>
+        </Link>
+        <Link href="#experience">
+          <button className="heroButton">Experience</button>
+        </Link>
+        <Link href="#skills">
+          <button className="heroButton">Skills</button>
+        </Link>
+        <Link href="#projects">
+          <button className="heroButton">Projects</button>
+        </Link>
+      </div>
+    </motion.div>
+  );
+}
