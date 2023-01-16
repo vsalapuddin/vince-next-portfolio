b270fd9dc2f871e89035aec3e77c62b8e8ec3546
Salapuddin <vsalapuddin@ufl.edu>
2023-01-15 21:17:15 -0500

    added framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://twitter.com/VinceSalapuddin"
           fgColor="gray"
@@ -22,9 +28,14 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         ></SocialIcon>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -34,7 +45,7 @@ export default function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-4">
           Contact Me
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
