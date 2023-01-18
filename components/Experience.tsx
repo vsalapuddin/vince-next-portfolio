fdf79abc372d3b076f22b3fdcb7642dfbec837ba
Salapuddin <vsalapuddin@ufl.edu>
2023-01-17 21:36:42 -0500

    scrollbar changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,7 +16,7 @@ export default function Experience({}: Props) {
         Experience
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#32CD32]">
         <ExperienceCarousel />
         <ExperienceCarousel />
         <ExperienceCarousel />
