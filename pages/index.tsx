5722743491d2f65afbab5892b065f1dce331cede
Salapuddin <vsalapuddin@ufl.edu>
2023-01-17 21:26:33 -0500

    custom scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import Contact from "@/components/Contact";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 ">
       <Head>
         <title>Vince's Portfolio</title>
       </Head>
