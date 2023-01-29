369227f960f62c2288c08a7397d16f98df15e69e
Salapuddin <vsalapuddin@ufl.edu>
2023-01-29 17:20:51 -0500

    added resume button

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,21 +30,35 @@ export default function Header({}: Props) {
           bgColor="transparent"
         ></SocialIcon>
       </motion.div>
-      <Link href="#contact" legacyBehavior>
-        <motion.div
-          initial={{ x: 500, opacity: 0, scale: 0.5 }}
-          animate={{ x: 0, opacity: 1, scale: 1 }}
-          transition={{ duration: 1.5 }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
-        >
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
-          />
-        </motion.div>
-      </Link>
+      <div className="flex">
+        <button className="text-gray-500 font-bold py-2 px-4 rounded-full inline-flex">
+          <svg
+            className="fill-current w-4 h-4 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+          </svg>
+          <a href="/VinceSalapuddinResume.pdf" download>
+            Resume
+          </a>
+        </button>
+        <Link href="#contact" legacyBehavior>
+          <motion.div
+            initial={{ x: 500, opacity: 0, scale: 0.5 }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5 }}
+            className="flex flex-row items-center text-gray-300 cursor-pointer"
+          >
+            <SocialIcon
+              className="cursor-pointer"
+              network="email"
+              fgColor="gray"
+              bgColor="transparent"
+            />
+          </motion.div>
+        </Link>
+      </div>
     </header>
   );
 }
