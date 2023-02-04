307d033cdfa4f791f001824d69d3fb52c455ebd9
Salapuddin <vsalapuddin@ufl.edu>
2023-02-03 22:21:57 -0500

    Made about me page work on 320px

--- components/About.tsx
+++ components/About.tsx
@@ -10,25 +10,25 @@ export default function About({}: Props) {
         initial={{ x: -200, opacity: 0 }}
         transition={{ duration: 1.6 }}
         whileInView={{ opacity: 1, x: 0 }}
-        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+        className="flex flex-col relative h-screen md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
       >
-        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        <h3 className="absolute top-24 uppercase tracking-[15px] md:tracking-[15px] lg:tracking-[15px] xl:tracking-[15px] text-gray-500 text-xl md:text-2xl lg:text-2xl xl:text-2xl">
           About Me
         </h3>
         <motion.img
           initial={{ x: -200, opacity: 0 }}
-          transition={{ duration: 2 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          src="/about.jpeg"
-          className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px]"
-        />
-        <motion.div
-          initial={{ x: 600, opacity: 0 }}
           transition={{ duration: 0.8 }}
           whileInView={{ opacity: 1, x: 0 }}
-          className="space-y-10 px-0 md:px-10"
+          src="/about.jpeg"
+          className=" mt-20 -mb-10 md:mt-0 lg:mt-0 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 lg:w-64 lg:h-64 xl:w-[400px] xl:h-[400px]"
+        />
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          transition={{ duration: 0.8 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          className="space-y-10 px-0 md:px-10 w-full"
         >
-          <p className="text-sm font-sans leading-loose">
+          <p className="text-sm md:text-sm lg:text-sm font-sans xl:leading-loose lg:leading-loose md:leading-loose ">
             Hello, my name is Vince Salapuddin and I am a highly motivated
             computer science graduate from the University of Florida. I am
             currently working as a software developer for the University of
