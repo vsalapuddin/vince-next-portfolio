405d4f965126fe2954fc6cf489e7c4ada0c68fae
Salapuddin <vsalapuddin@ufl.edu>
2023-02-07 00:49:51 -0500

    Added more skills and reformatted them and changed the tab name

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,7 +11,7 @@ const skills = [
   },
   {
     src: "node",
-    count: 45,
+    count: 55,
     title: "Node",
   },
   {
@@ -34,6 +34,31 @@ const skills = [
     count: 60,
     title: "SQL",
   },
+  {
+    src: "vue",
+    count: 55,
+    title: "Vue",
+  },
+  {
+    src: "postman",
+    count: 70,
+    title: "Postman",
+  },
+  {
+    src: "python",
+    count: 45,
+    title: "Python",
+  },
+  {
+    src: "javascript",
+    count: 85,
+    title: "Javascript",
+  },
+  {
+    src: "java",
+    count: 35,
+    title: "Java",
+  },
 ];
 
 function Skills({}: Props) {
@@ -44,11 +69,11 @@ function Skills({}: Props) {
       whileInView={{ opacity: 1, x: 0 }}
       className="h-screen"
     >
-      <div className="max-w-screen-log mx-auto p-4 flex flex-col justify-center w-full h-full text-gray-500">
+      <div className="max-w-screen-md mx-auto p-4 flex flex-col justify-center h-full text-gray-500">
         <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl text-center">
           Skills
         </h3>
-        <div className="flex items-center justify-center mt-8 gap-6 flex-wrap">
+        <div className="mt-8 grid grid-cols-5 flex-wrap">
           {skills?.map((skill, i) => (
             <div key={i} className="text-center">
               <motion.div
@@ -58,20 +83,19 @@ function Skills({}: Props) {
                 style={{
                   background: `conic-gradient(rgb(50,205,50) ${skill.count}%, transparent ${skill.count}%)`,
                 }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
+                className="w-24 h-24 flex items-center justify-center rounded-full"
               >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center">
+                <div className="text-6xl w-20 h-20 bg-gray-900 rounded-full flex items-center justify-center">
                   <motion.img
                     initial={{ x: -200, opacity: 0 }}
                     transition={{ duration: 1 }}
                     whileInView={{ x: 0, opacity: 1 }}
                     src={`/iconSkills/${skill.src}.png`}
                     alt="skills"
-                    className="w-20 mx-auto"
                   />
                 </div>
               </motion.div>
-              <p className="text-xl mt-3">{skill.title}</p>
+              <p className="mt-3">{skill.title}</p>
             </div>
           ))}
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 no-scrollbar">
       <Head>
-        <title>Vince's Portfolio</title>
+        <title>Vince Salapuddin</title>
       </Head>
       <Header />
       <section id="hero" className="snap-start">
