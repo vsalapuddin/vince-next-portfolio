241c080de3e2b1774d210dc178014eea469b37c0
Salapuddin <vsalapuddin@ufl.edu>
2023-02-07 01:26:04 -0500

    Finished Skills section, compatible 320px, added all skills, changed appearance

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,48 +6,8 @@ type Props = {};
 const skills = [
   {
     src: "react",
-    count: 75,
+    count: 85,
     title: "React",
-  },
-  {
-    src: "node",
-    count: 55,
-    title: "Node",
-  },
-  {
-    src: "matui",
-    count: 75,
-    title: "Material Ui",
-  },
-  {
-    src: "git",
-    count: 85,
-    title: "Git",
-  },
-  {
-    src: "c++",
-    count: 85,
-    title: "C++",
-  },
-  {
-    src: "sql",
-    count: 60,
-    title: "SQL",
-  },
-  {
-    src: "vue",
-    count: 55,
-    title: "Vue",
-  },
-  {
-    src: "postman",
-    count: 70,
-    title: "Postman",
-  },
-  {
-    src: "python",
-    count: 45,
-    title: "Python",
   },
   {
     src: "javascript",
@@ -55,9 +15,54 @@ const skills = [
     title: "Javascript",
   },
   {
-    src: "java",
-    count: 35,
-    title: "Java",
+    src: "c++",
+    count: 85,
+    title: "C++",
+  },
+  {
+    src: "git",
+    count: 85,
+    title: "Git",
+  },
+  {
+    src: "html",
+    count: 75,
+    title: "HTML",
+  },
+  {
+    src: "css",
+    count: 70,
+    title: "CSS",
+  },
+  {
+    src: "matui",
+    count: 70,
+    title: "Material Ui",
+  },
+  {
+    src: "postman",
+    count: 70,
+    title: "Postman",
+  },
+  {
+    src: "sql",
+    count: 60,
+    title: "SQL",
+  },
+  {
+    src: "node",
+    count: 55,
+    title: "Node",
+  },
+  {
+    src: "vue",
+    count: 55,
+    title: "Vue",
+  },
+  {
+    src: "python",
+    count: 45,
+    title: "Python",
   },
 ];
 
@@ -69,34 +74,37 @@ function Skills({}: Props) {
       whileInView={{ opacity: 1, x: 0 }}
       className="h-screen"
     >
-      <div className="max-w-screen-md mx-auto p-4 flex flex-col justify-center h-full text-gray-500">
+      <div className="max-w-screen-md mx-auto p-4 items-center flex flex-col justify-center h-full text-gray-500">
         <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl text-center">
           Skills
         </h3>
-        <div className="mt-8 grid grid-cols-5 flex-wrap">
+        <div className="mt-8 grid grid-cols-4 flex-wrap">
           {skills?.map((skill, i) => (
-            <div key={i} className="text-center">
-              <motion.div
-                initial={{ x: 200, opacity: 0 }}
-                transition={{ duration: 1.6 }}
-                whileInView={{ opacity: 1, x: 0 }}
+            <motion.div
+              initial={{ x: 200, opacity: 0 }}
+              transition={{ duration: 1.6 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              key={i}
+              className="text-center mx-4 my-1"
+            >
+              <div
                 style={{
                   background: `conic-gradient(rgb(50,205,50) ${skill.count}%, transparent ${skill.count}%)`,
                 }}
-                className="w-24 h-24 flex items-center justify-center rounded-full"
+                className="w-16 h-16 md:max-2xl:w-24 md:max-2xl:h-24 flex items-center justify-center rounded-full"
               >
-                <div className="text-6xl w-20 h-20 bg-gray-900 rounded-full flex items-center justify-center">
+                <div className="text-6xl p-2 md:max-2xl:p-1.5 w-12 h-12 md:max-2xl:w-20 md:max-2xl:h-20 bg-gray-900 rounded-full flex items-center justify-center">
                   <motion.img
-                    initial={{ x: -200, opacity: 0 }}
+                    initial={{ opacity: 0 }}
                     transition={{ duration: 1 }}
-                    whileInView={{ x: 0, opacity: 1 }}
+                    whileInView={{ opacity: 1 }}
                     src={`/iconSkills/${skill.src}.png`}
                     alt="skills"
                   />
                 </div>
-              </motion.div>
-              <p className="mt-3">{skill.title}</p>
-            </div>
+              </div>
+              <p className="mt-3 text-sm md:max-2xl:text-lg">{skill.title}</p>
+            </motion.div>
           ))}
         </div>
       </div>
