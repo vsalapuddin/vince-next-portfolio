af16ed6f1017bece0e2ca757d6339d127f5e0246
Salapuddin <vsalapuddin@ufl.edu>
2023-02-14 22:12:13 -0500

    Fixed Projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,35 +8,30 @@ import "swiper/css/pagination";
 export default function Projects({}) {
   const projects = [
     {
+      name: "Web3 Kahoot",
+      desc: `Developed a time-entry application for employees for faster, more accurate timesheets. Utilized Vue as well as multiple API integrations for a clean and efficient user experience.`,
+    },
+    {
       name: "OpenAir Project",
       desc: `Developed a time-entry application for employees for faster, more accurate timesheets. Utilized Vue as well as multiple API integrations for a clean and efficient user experience.`,
-      github_link: "https://github.com/Sridhar-C-25",
-      live_link: "https://myreactflix.netlify.app",
     },
     {
       name: "UF Directory App",
       desc: "Developed a directory app using the MERN (MongoDB, Express, Angular, Node.js) stack. Utilized git as version control for this full-stack web app.",
-      github_link: "https://github.com/Sridhar-C-25/jobsearchapp",
-      live_link: "https://myjobsearch.netlify.app",
     },
     {
       name: "Minesweeper",
       desc: "C++ with Simple and Fast Multimedia Library to create a standard minesweeper game. ",
-      github_link: "https://github.com/Sridhar-C-25/highking",
-      live_link: "https://highking01.netlify.app",
     },
     {
       name: "File I/O Quality Checker ",
       desc: "Developed a file I/O program using C++ to quality check excel files for errors (created to increase job efficiency for myself and other workers at my place of work).",
-      github_link:
-        "https://github.com/Sridhar-C-25/reacttailwindnavbar-with-dropdown",
-      live_link: "https://reacttailwindnavbar.netlify.app",
+      github_link: "https://github.com/vsalapuddin/Quality-Checker",
     },
     {
       name: "Trader Gator",
       desc: "Informational website teaching users how to trade options in the stock market. (HTML and CSS)",
-      github_link: "https://github.com/Sridhar-C-25",
-      live_link: "https://vuecountry05.netlify.app",
+      github_link: "https://github.com/vsalapuddin/vsalapuddin.github.io",
     },
   ];
   return (
@@ -44,47 +39,33 @@ export default function Projects({}) {
       initial={{ x: 200, opacity: 0 }}
       transition={{ duration: 1 }}
       whileInView={{ opacity: 1, x: 0 }}
-      className="h-screen py-10 text-white"
+      className="h-screen"
     >
-      <div className="max-w-screen-log mx-auto p-4 flex flex-col justify-center w-full h-full text-white-500">
+      <div className="p-0 md:p-4 flex flex-col justify-center w-full h-full text-white-500">
         <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl text-center">
           Projects
         </h3>
-        <div className="flex max-w-3xl gap-6 px-5 mx-auto items-center relative">
+        <div className="flex max-w-xs md:max-w-3xl gap-6 mx-auto items-center relative">
           <div className="w-full">
             <Swiper
               slidesPerView={1}
               spaceBetween={0}
+              pagination={{
+                clickable: true,
+              }}
               loop={true}
               autoplay={{
                 delay: 3000,
               }}
-              pagination={{
-                clickable: true,
-              }}
               modules={[Pagination, Autoplay]}
             >
               {projects.map((project_info, i) => (
-                <SwiperSlide key={i}>
-                  <div className="h-48 w-300 p-4 bg-[#292929] rounded-xl position-absolute">
-                    <h3 className="text-xl my-4">{project_info.name}</h3>
+                <SwiperSlide key={i} className="mb-10">
+                  <div className="h-48 p-4 bg-[#292929] rounded-xl position-absolute">
+                    <h3 className="text-xl my-0 md:my-4">
+                      {project_info.name}
+                    </h3>
                     <p>{project_info.desc}</p>
-                    <div className="flex gap-3">
-                      <a
-                        href={project_info.github_link}
-                        target="_blank"
-                        className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
-                      >
-                        Github
-                      </a>
-                      <a
-                        href={project_info.live_link}
-                        target="_blank"
-                        className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
-                      >
-                        Live Demo
-                      </a>
-                    </div>
                   </div>
                 </SwiperSlide>
               ))}
