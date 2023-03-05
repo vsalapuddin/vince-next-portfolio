fb81bb40c14a2d74bed2f6e435a4a1b9b020cdb3
Salapuddin <vsalapuddin@ufl.edu>
2023-03-05 13:19:52 -0500

    Added images to about me section

--- components/About.tsx
+++ components/About.tsx
@@ -17,29 +17,44 @@ export default function About({}: Props) {
       <div className="border p-4 rounded-lg">
         <div className="grid grid-rows-3 gap-4 max-w-xl">
           <div className="border p-4 rounded-lg">
-            <h2 className="text-lg font-bold mb-2">Software Development</h2>
-            <p className="text-gray-500">
-              Experienced in both functional and OOP: C++, JavaScript,
-              TypeScript.
-            </p>
+            <div className="flex flex-row">
+              <img className="h-16 w-16 m-2" src="/monitor.png" />
+              <div>
+                <h2 className="text-lg font-bold mb-2">Software Development</h2>{" "}
+                <p className="text-gray-500">
+                  Experienced in both functional and OOP: C++, JavaScript,
+                  TypeScript.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="border p-4 rounded-lg">
-            <h2 className="text-lg font-bold mb-2">
-              Frontend Dev React, NextJS
-            </h2>
-            <p className="text-gray-500">
-              Passionate about UI/UX. Experience in HTML, CSS, JS, React and
-              NextJS frameworks.
-            </p>
+            <div className="flex flex-row">
+              <img className="h-16 w-16 m-2" src="/iconSkills/react.png" />
+              <div>
+                <h2 className="text-lg font-bold mb-2">
+                  Frontend Dev React, NextJS
+                </h2>
+                <p className="text-gray-500">
+                  Passionate about UI/UX. Experience in HTML, CSS, JS, React and
+                  NextJS frameworks.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="border p-4 rounded-lg">
-            <h2 className="text-lg font-bold mb-2">
-              React Native Dev Android, iOS
-            </h2>
-            <p className="text-gray-500">
-              Skilled in developing hybrid mobile apps and cross-platform
-              solutions using the React Native framework.
-            </p>
+            <div className="flex flex-row">
+              <img className="h-16 w-16 m-2" src="/phone.png" />
+              <div>
+                <h2 className="text-lg font-bold mb-2">
+                  React Native Dev Android, iOS
+                </h2>
+                <p className="text-gray-500">
+                  Skilled in developing hybrid mobile apps and cross-platform
+                  solutions using the React Native framework.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
